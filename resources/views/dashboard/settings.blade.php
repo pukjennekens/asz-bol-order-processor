909ab36ae86a4ca88b64dd3d33129a63eb35bc48
Gerard <41292403+gerardpaardenkoper@users.noreply.github.com>
2023-12-21 16:17:48 +0100

    Added some security tweaks, dont show the client ID and secret to the customer anymore

--- resources/views/dashboard/settings.blade.php
+++ resources/views/dashboard/settings.blade.php
@@ -32,8 +32,8 @@
                                     @foreach(\App\Models\BolAccount::all() as $bolAccount)
                                         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                             <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $bolAccount->name }}</td>
-                                            <td class="px-6 py-4">{{ $bolAccount->client_id }}</td>
-                                            <td class="px-6 py-4">{{ $bolAccount->client_secret }}</td>
+                                            <td class="px-6 py-4">{{ substr($bolAccount->client_id, 0, 8) }}<strong>****</strong></td>
+                                            <td class="px-6 py-4">{{ substr($bolAccount->client_secret, 0, 8) }}<strong>****</strong></td>
                                             <td>
                                                 <a href="{{ route('dashboard.settings.delete', $bolAccount->id) }}">
                                                     <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path opacity="1" fill="#ef4444" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
